62296a301cbe9b5c6b9e1f5fa776cc10a17e8bcd
Todorokix <157006561+Todorokix@users.noreply.github.com>
2024-05-24 19:02:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,51 @@
 <?php
 error_reporting(0);
 echo " HAPPY LOOTING!! \n";
+
 unlink('cookie.txt');
 
 
-$mail = array("kongoytre@embeans.com");
+$n=4;
 
+function getName($n) {
+    $characters = '0123456789';
+    $randomString = '';
+ 
+    for ($i = 0; $i < $n; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $randomString .= $characters[$index];
+    }
+ 
 
-$gg = rand(1,100);
-$xc = rand(1,100);
-$ipp = "".$gg.".106.9.".$xc;
+    return $randomString;
+}
 
-function curl_request($url, $method, $data = null) {
-	global $ipp,$vvv;
+$mnk = getName($n);
+$rd = rand(0,999);
+$vvv = "Mozilla/5.0 (Linux; Android 13; SM-A515U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36 X/".$mnk."";
+function generateRandomIP() {
+    // Generate random values for each octet
+    $octet1 = rand(1, 255);
+    $octet2 = rand(0, 255);
+    $octet3 = rand(0, 255);
+    $octet4 = rand(1, 255);
+
+    // Combine the octets into a single IP address string
+    $randomIP = "$octet1.$octet2.$octet3.$octet4";
+
+    return $randomIP;
+}
+$ipx = generateRandomIP();
+$ipxx = generateRandomIP();
+function curl($url, $method, $data = null) {
+	global $ipx, $vvv;
     $header = array(
-        "Host: megaclaimer.com",
-        "X-Forwarded-For: ".$ipp."",
-        "upgrade-insecure-requests: 1",
+        "Host: cryptovertz.com",
+        "origin: https://cryptovertz.com",
         "content-type: application/x-www-form-urlencoded",
-        "X-Requested-With: XMLHttpRequest",      
-        "User-Agent: ".$vvv.""
+        "X-Requested-With: XMLHttpRequest",
+        "X-Forwarded-For: $ipx",
+        "user-agent: $vvv"
     );
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -28,7 +54,7 @@ function curl_request($url, $method, $data = null) {
     curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_COOKIE, "");
+    curl_setopt($ch, CURLOPT_COOKIE,TRUE);     
     curl_setopt($ch, CURLOPT_COOKIEFILE,"cookie.txt");
     curl_setopt($ch, CURLOPT_COOKIEJAR,"cookie.txt");
     if ($method === 'POST') {
@@ -39,11 +65,41 @@ function curl_request($url, $method, $data = null) {
     curl_close($ch);
     return $result;
 }
+
+function curl_request($url, $method, $data = null) {
+	global $ipx, $vvv;
+    $header = array(
+        "Host: flukelabs.com",
+        "origin: https://flukelabs.com",
+        "content-type: application/x-www-form-urlencoded",
+        "X-Requested-With: XMLHttpRequest",
+        "X-Forwarded-For: $ipx",
+        "user-agent: $vvv"
+    );
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_COOKIE,TRUE);     
+    curl_setopt($ch, CURLOPT_COOKIEFILE,"cookie.txt");
+    curl_setopt($ch, CURLOPT_COOKIEJAR,"cookie.txt");
+    if ($method === 'POST') {
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    }
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+
 function solveCaptcha(){
 	global $vvv;
 a:
-$sit = "ec70f443-7e6a-49ad-9073-3a416198b15e";
-$login = "http://sctg.xyz/in.php?key=pgaGommLr1eenOsedtdL92F3vOzCfNSA&method=hcaptcha&sitekey=".$sit."&json=1&pageurl=https://megaclaimer.com/login.php";
+$sit = "6LdpUzMnAAAAAEFrPfwSJ7FA1H31hZ4-2e-rAsx7";
+$login = "http://api.sctg.xyz/in.php?key=Gjd5MbFADqP0DlrurYrAmdIlQ9owqctV|onlyxevil&method=userrecaptcha&googlekey=".$sit."&json=1&pageurl=https://cryptovertz.com/rewards/reward.php";
 $ua[] = "User-Agent: ".$vvv."";
 $ua[] = "Content-Type: application/json";
 $ch = curl_init();
@@ -58,13 +114,17 @@ $re = json_decode($result);
 $id = $re->request;
 if($id==''){goto a;}
 c:
-$url = "http://sctg.xyz/res.php?key=pgaGommLr1eenOsedtdL92F3vOzCfNSA&action=get&id=".$id."";
+$url = "http://api.sctg.xyz/res.php?key=Gjd5MbFADqP0DlrurYrAmdIlQ9owqctV|onlyxevil&action=get&id=".$id."";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $ua);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+$proxy = 'socks5://gvtzeurj:0cequgcvyowo@38.154.227.167:5868';
+curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);       
+curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
 $res = curl_exec($ch);
 
 if ($res == 'CAPCHA_NOT_READY') {          
@@ -76,47 +136,45 @@ $captcha = str_replace("OK|", "", $res);
 curl_close($ch);
 return $captcha;
 }
-
-
-
-$repp = $mail[0];
-
-
-$url = "https://megaclaimer.com/login.php";
+$url = "https://flukelabs.com/pullfluke?partner=freebonk&user_id=47648";
+//$url = "https://flukelabs.com/pullfluke?partner=fakechicken&user_id=17867";
 $response = curl_request($url, 'GET');
-$capp = solveCaptcha();
-$url = 'https://megaclaimer.com/login.php';
-$data = "email=".$repp."&password=Nung1234&g-recaptcha-response=&h-captcha-response=".$capp."&submit=login+Now";
-$response = curl_request($url, 'POST', $data);
-//echo $response;exit;
 
-$url = 'https://megaclaimer.com/member/account.php';
+$url = "https://flukelabs.com/ajax/fetchGames";
 $res = curl_request($url, 'GET');
-$id = explode('">' ,explode('<input type="hidden" name = "token" value="', $res)[1])[0];
-$bal = explode(" coins" ,explode("<i class='bx bxs-coin-stack' style='color:#ced215;height:100%;'  ></i><span> ", $res)[1])[0];
-$mma = explode('" >' ,explode('<input type="text" name="fpemail" value="', $res)[1])[0];
-/*
-if($mma == ""){
+$link = explode("', this" ,explode("redirectToAboutBlank('", $res)[1])[0];
 
-$url = 'https://megaclaimer.com/member/account.php';
-$data = "fpemail=artia.zana88@gmail.com&cwemail=&payeer=&bitcoin=&token=".$id."&update=Update";
-$response = curl_request($url, 'POST', $data);
-$ma = explode('" >' ,explode('<input type="text" name="fpemail" value="', $response)[1])[0];
-
-echo " ".$ma." \n";
-}
-*/
-$url = 'https://megaclaimer.com/member/withdraw.php';
-$res = curl_request($url, 'GET');
-
-$url = 'https://megaclaimer.com/member/withdraw-verify.php';
-$data = "currency=bitcoin&address=artia.zana88@gmail.com&amount=".$bal."&fp-withdrawal=Withdraw";
-$response = curl_request($url, 'POST', $data);
-$ball = explode(" coins" ,explode("<i class='bx bxs-coin-stack' style='color:#ced215;height:100%;'  ></i><span> ", $response)[1])[0];
-echo "Balance = ".$ball." \n";
-sleep(99999);
+$gam = curl($link, 'GET');
 
 
+$url = "https://cryptovertz.com/";
+$data = "submit=Continue";
+$response = curl($url, 'POST', $data);
+
+p:
+$url = "https://cryptovertz.com/game.php?id=boxing-stars";
+$str = curl($url, 'GET');
+$lef = explode(' / 50</button>',explode('<button class="btn btn-danger">Limit: ', $str)[1])[0];
+
+sleep(71);
+$capv = solveCaptcha();
 
 
+$url = 'https://cryptovertz.com/rewards/reward.php';
+$data = "rcapchta_response=".$capv."";
+$response = curl($url, 'POST', $data);
 
+$suc = explode("<a class='btn btn-primary' href='rewards/start.php?",$response);
+$ren = $suc[0];
+if (strpos($response, "limit reached") !== false) {echo "Complete!!! \n";unlink('cookie.txt');sleep(99999);}
+$user = explode('&crypto',explode('user=', $response)[1])[0];
+date_default_timezone_set('Asia/Jakarta');
+$timestamp = time();
+$wak = date("[H:i]", $timestamp);
+echo" ".$wak." ".$ren." \n";
+$url = "https://cryptovertz.com/rewards/start.php?partner=flukegames&user=".$user."&crypto=SHIB";
+$str = curl($url, 'GET');
+//if($lef == "49"){echo "Complete!!! \n";unlink('cookie.txt');sleep(99999);}
+goto p;
+
+?>
