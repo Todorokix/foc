b35f62e1d0ede54d4290a1db90d43f1e108978b6
Todorokix <157006561+Todorokix@users.noreply.github.com>
2024-06-05 17:17:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,11 @@
 <?php
 error_reporting(0);
 echo " HAPPY LOOTING!! \n";
-
-function http_request($url, $method = 'GET', $data = null, $headers = []) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    if (!empty($headers)) {
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    }
-    if (strtoupper($method) === 'POST') {
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    }
-    $response = curl_exec($ch);
-    if (curl_errno($ch)) {
-        $error_msg = curl_error($ch);
-        curl_close($ch);
-        return "cURL Error: $error_msg";
-    }
-    curl_close($ch);
-    return $response;
-}
-
 function solveCaptcha(){
 	global $vvv;
 a:
-$sit = "6LfqFAcdAAAAAKWiUVv3EkT0le7pDL6lnsGfe5l6";
-$login = "http://sctg.xyz/in.php?key=Gjd5MbFADqP0DlrurYrAmdIlQ9owqctV&method=userrecaptcha&googlekey=".$sit."&json=1&pageurl=https://ourcoincash.xyz/faucet/verify";
+$sit = "6LdpUzMnAAAAAEFrPfwSJ7FA1H31hZ4-2e-rAsx7";
+$login = "http://api.sctg.xyz/in.php?key=Gjd5MbFADqP0DlrurYrAmdIlQ9owqctV&method=userrecaptcha&googlekey=".$sit."&json=1&pageurl=https://cryptovertz.com/rewards/reward.php";
 $ua[] = "User-Agent: ".$vvv."";
 $ua[] = "Content-Type: application/json";
 $ch = curl_init();
@@ -68,28 +43,101 @@ $captcha = str_replace("OK|", "", $hy);
 curl_close($ch);
 return $captcha;
 }
-while(true):
+function http_request($url, $method = 'GET', $data = null, $headers = []) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+    curl_setopt($ch, CURLOPT_COOKIEFILE,"cookie.txt");
+    curl_setopt($ch, CURLOPT_COOKIEJAR,"cookie.txt");
+    if (!empty($headers)) {
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    }
+    if (strtoupper($method) === 'POST') {
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    }
+    $response = curl_exec($ch);
+    if (curl_errno($ch)) {
+        $error_msg = curl_error($ch);
+        curl_close($ch);
+        return "cURL Error: $error_msg";
+    }
+    curl_close($ch);
+    return $response;
+}
+
+$n=4;
+function getName($n) {
+    $characters = '0123456789';
+    $randomString = ''; 
+    for ($i = 0; $i < $n; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $randomString .= $characters[$index];
+    }
+    return $randomString;
+}
+
+$mnk = getName($n);
+$rd = rand(0,999);
+$vvv = "Mozilla/5.0 (Linux; Android 13; SM-A515U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36 X/".$mnk."";
+function generateRandomIP() {
+    $octet1 = rand(1, 255);
+    $octet2 = rand(0, 255);
+    $octet3 = rand(0, 255);
+    $octet4 = rand(1, 255);
+    $randomIP = "$octet1.$octet2.$octet3.$octet4";
+    return $randomIP;
+}
+$ipx = generateRandomIP();
+$ipxx = generateRandomIP();
+
+
+x:
+$ar= array("6JZzRosxGZ");
+$user = $ar[0];
+
 $headers = [
-    "Host: ourcoincash.xyz",
-    "content-type: application/x-www-form-urlencoded",
-    "User-Agent: Mozilla/5.0 (Linux; Android 11; V2043) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
-    "Cookie: ci_session=d9cfeb873e043a3e7585c8fa039a2c5c109fd295; cf_clearance=A2jEGDDAfqzCjku7tGXclLw7fChE.i4DZg4DjfhLCd8-1717571140-1.0.1.1-1YtiK3ixuPtHyqBefe2JL6tSGuONcHzQ1hSZE2fg5hV_5e8p3RQfzMOsy3B09DfZcknRs3fHd2urIAvn6aRGZw; zone-cap-5074476=1;1717571142; csrf_cookie_name=9641e9fed1327ad6b365b56582c6d739; _ga=GA1.1.2119012172.1717571163; _gcl_au=1.1.2029378879.1717571164; _ga_VN4LQWVHKZ=GS1.1.1717571163.1.1.1717571183.0.0.0"
+       "Host: cryptovertz.com",
+        "origin: https://cryptovertz.com",
+        "content-type: application/x-www-form-urlencoded",
+        "X-Requested-With: XMLHttpRequest",
+        "X-Forwarded-For: $ipx, $ipxx",      
+        "user-agent: $vvv"
 ];
-$url = "https://ourcoincash.xyz/faucet";
-$res = http_request($url, 'GET', null, $headers);
-$csf = explode('">',explode('<input type="hidden" name="csrf_token_name" id="token" value="', $res)[1])[0];
-$tok = explode('">',explode('<input type="hidden" name="token" value="', $res)[1])[0];
-if($csf == ""){echo "csf hilang \n";sleep(99999);}
-$capp = solveCaptcha();
-
-$url = "https://ourcoincash.xyz/faucet/verify";
-$data = "csrf_token_name=".$csf."&token=".$tok."&captcha=recaptchav2&g-recaptcha-response=".$capp."";
+$url = "https://cryptovertz.com/rewards/start.php?partner=flukegames&user=".$user."&crypto=SHIB";
+$str = http_request($url, 'GET', null, $headers);
+$url = "https://cryptovertz.com/";
+$data = "submit=Continue";
 $response = http_request($url, 'POST', $data, $headers);
-$lef = explode('</h4>',explode('<h4 class="lh-1 mb-1">', $response)[4])[0];
-if($lef == "1/200"){sleep(99999);}
-echo " [".$lef."]  \n";
-sleep(10);
+
+while(true):
+$url = "https://cryptovertz.com/game.php?id=boxing-stars";
+$str = http_request($url, 'GET', null, $headers);
+$lef = explode(' / 50</button>',explode('<button class="btn btn-danger">Limit: ', $str)[1])[0];
+
+sleep(71);
+$capv = solveCaptcha();
+
+$url = 'https://cryptovertz.com/rewards/reward.php';
+$data = "rcapchta_response=".$capv."";
+$response = http_request($url, 'POST', $data, $headers);
+
+$suc = explode("<a class='btn btn-primary' href='rewards/start.php?",$response);
+$ren = $suc[0];
+if (strpos($response, "Invalid Claim") !== false) {goto x;}
+if (strpos($response, "FAILED") !== false) {goto x;}
+if (strpos($response, "limit reached") !== false) {echo "Complete!!! \n";unlink('cookie.txt');sleep(99999);}
+//$user = explode('&crypto',explode('user=', $response)[1])[0];
+date_default_timezone_set('Asia/Jakarta');
+$timestamp = time();
+$wak = date("[H:i]", $timestamp);
+if (strpos($response, "been paid") !== false) {echo" ".$wak." ".$ren." : $user\n";}else{echo "Claim gagal! \n";goto x;}
+$url = "https://cryptovertz.com/rewards/start.php?partner=flukegames&user=".$user."&crypto=SHIB";
+$str = http_request($url, 'GET', null, $headers);
+//if($lef == "49"){echo "Complete!!! \n";unlink('cookie.txt');sleep(99999);}
 endwhile;
-
-
 ?>
